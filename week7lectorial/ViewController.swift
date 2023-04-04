8b097ae3d90eb2ed1504fa6a721951a16bde9b31
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-04-04 10:33:12 +1000

    Initial Commit

---
+++ week7lectorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week7lectorial
+//
+//  Created by mobiledev on 4/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
